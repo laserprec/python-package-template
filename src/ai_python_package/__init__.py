8030e0833f54c0a9701e6bacc77ce45eac6e8b55
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-13 03:49:52 -0400

    Create pyproject.toml
    Update pypi-test-publish.yml
    
    Update pypi-publish.yml
    
    Delete black.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update tox.ini
    
    Update tox.ini
    
    Update pyproject.toml
    
    Update black.yml
    
    Update black.yml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update black.yml
    
    rename python project dir with underscore
    
    Update tests/test_methods.py
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    formatting
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Delete mypy.yml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyright.yml
    
    Update pytest.yml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update __init__.py
    
    Update ai_python_package/__init__.py
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Apply suggestions from code review
    
    Co-authored-by: Steve Dower <steve.dower@microsoft.com>
    Co-authored-by: Brett Cannon <brett@python.org>
    
    Update .github/workflows/black.yml
    
    Delete tox.ini
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    Update pypi-publish.yml
    
    Update pytest.yml
    
    Create bandit.yml
    
    Rename main.yml to wemake-python-styleguide.yml
    
    Update pyproject.toml
    
    Co-authored-by: Steve Dower <steve.dower@microsoft.com>
    
    create source directory
    
    Update pyproject.toml
    
    Update pyproject.toml
    
    cleanup
    
    Update pyright.yml
    
    Update pyright.yml
    
    Update pytest.yml
    
    Update pytest.yml
    
    Update bandit.yml
    
    Update bandit.yml
    
    Update black.yml
    
    Update pylint.yml

---
+++ src/ai_python_package/__init__.py
@@ -0,0 +1,5 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Python Package Template"""
